8d006cf080136e4c7abcfeb1f05dab1361a86bce
Watscho Aiwasov <w4tscho@gmail.com>
2021-07-27 19:08:00 +0400

    fix: rootSaga forks definition

--- workspaces/app/src/js/redux/sagas.js
+++ workspaces/app/src/js/redux/sagas.js
@@ -3,7 +3,7 @@ import { all, fork } from 'redux-saga/effects'
 import { sagas } from 'modules'
 
 export const rootSaga = function* () {
-  const forks = []
+  let forks = []
 
   for (let i = 0; i < sagas.length; i++) {
     forks.push(fork(sagas[i]))
